0d4db24de0d28b6c09821eef5a02404e486fb0b2
Steve Shogren <steve.a.shogren@gmail.com>
2013-05-20 17:05:00 -0400

    adding the factorial and fib exercises

--- chapt1.rkt
+++ chapt1.rkt
@@ -104,5 +104,40 @@
 
 (sqrt-lex 9)
 
+(define (factorial n)
+  (if (= n 1)
+      1
+      (* n (factorial (- n 1)))))
+(factorial 50)
+
+(define (factorial n)
+  (fact-iter 1 1 n))
+
+(define (fact-iter product counter max-count)
+  (if (> counter max-count)
+      product
+      (fact-iter (* counter product)
+                 (+ counter 1)
+                 max-count)))
+
+(factorial 40)
+
+;; 1.10
+(define (A x y)
+  (cond ((= y 0) 0)
+        ((= x 0) (* 2 y))
+        ((= y 1) 2)
+        (else (A (- x 1)
+                 (A x (- y 1))))))
+(A 1 10) ;;  1024
+(A 2 4) ;; 65536
+(A 3 3) ;; 65536
+
+(define (fib n)
+  (cond ((= n 0) 0)
+        ((= n 1) 1)
+        (else (+ (fib (- n 1))
+                 (fib (- n 2))))))
+
 
 
