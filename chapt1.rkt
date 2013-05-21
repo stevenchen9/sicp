a51514edf6d24a61d9c2047f5cba71de5a22c614
Steve Shogren <steve.a.shogren@gmail.com>
2013-05-21 16:55:45 -0400

    an iterative fib function

--- chapt1.rkt
+++ chapt1.rkt
@@ -139,5 +139,12 @@
         (else (+ (fib (- n 1))
                  (fib (- n 2))))))
 
+(define (fib n)
+  (fib-iter 1 0 n))
+(define (fib-iter a b count)
+  (if (= count 0)
+      b
+      (fib-iter (+ a b) a (- count 1))))
+(fib 6)
 
 
