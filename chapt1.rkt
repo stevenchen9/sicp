63a5249429557b0bde39c5a9629d7a9ae8c5cbf8
Steve Shogren <steve.a.shogren@gmail.com>
2013-05-28 17:14:19 -0400

    Add exercise 1.15

--- chapt1.rkt
+++ chapt1.rkt
@@ -199,9 +199,9 @@
 (f 12) ;;1892 
 
 ;; 1.12
-;;    1
-;;   1 1
-;;  1 2 1
+;;    1  
+;;   1 1  
+;;  1 2 1  
 ;; 1 3 3 1
 ;;1 4 6 4 1
 (define (pascal-triangle r c)
@@ -215,6 +215,18 @@
        (assert-equal (pascal-triangle 5 3) 6)
        (assert-equal (pascal-triangle 1 1) 1))
 
+;; 1.15
+(define (cube x) (* x x x))
+(define (p x)
+  (print "x")
+  (- (* 3 x) ( * 4 (cube x))))
+(define (sine angle)
+  (if (not (> (abs angle) 0.1))
+      angle
+      (p (sine (/ angle 3.0)))))
+(sine 12.15) ; p -> 5 times
+(sine 30) ; p -> 6 times
+(sine 100) ; p -> 7 times
 
 
 
