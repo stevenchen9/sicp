d09b9999f6a8afc3d6d3ba14f2b1cca079b427cd
Steve Shogren <steve.a.shogren@gmail.com>
2013-07-17 20:13:42 -0400

    Add functions for prime number testing

--- chapt1.rkt
+++ chapt1.rkt
@@ -317,4 +317,25 @@
       (gcd b (remainder a b))))
 (gcd 206 40) ;; 2
 
+(define (smallest-divisor n)
+  (find-divisor n 2))
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+(prime? 4)
+
+(define (expmost base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (square (expmod base (/ exp 2) m))
+                    m))
+        (else
+         (remainder (* base (expmod base (- exp 1) m))
+                    m))))
 
