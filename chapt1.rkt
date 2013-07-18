501286676cad1ca95c2949964c615fa8cbfbddd1
Steve Shogren <steve.a.shogren@gmail.com>
2013-07-18 17:04:53 -0400

    A test for Fermat's Little Theorem to prove it works as expected

--- chapt1.rkt
+++ chapt1.rkt
@@ -330,7 +330,7 @@
   (= n (smallest-divisor n)))
 (prime? 4)
 
-(define (expmost base exp m)
+(define (expmod base exp m)
   (cond ((= exp 0) 1)
         ((even? exp)
          (remainder (square (expmod base (/ exp 2) m))
@@ -338,4 +338,16 @@
         (else
          (remainder (* base (expmod base (- exp 1) m))
                     m))))
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) true)
+        ((fermat-test n) (fast-prime? n (- times 1)))
+        (else false)))
+
+(fast-prime? 5 100)
 
