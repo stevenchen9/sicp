b640c20625bf69f563ec8ad9bad9b8005c1358bd
Steve Shogren <steve.a.shogren@gmail.com>
2013-07-24 17:11:03 -0400

    A fixed-point function that approximates the value of an equation

--- chapt1.rkt
+++ chapt1.rkt
@@ -576,3 +576,20 @@
                       1.0
                       2.0)
 
+(define tolerance 0.00001)
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+(fixed-point cos 1.0)
+
+(fixed-point (lambda (y) (+ (sin y) (cos y)))
+             1.0)
+
+
