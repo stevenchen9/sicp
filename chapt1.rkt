840cb1654fb1defd58e90212619ff7e71ae676da
Steve Shogren <steve.a.shogren@gmail.com>
2013-07-26 17:05:48 -0400

    Using the 'return function' abstraction to clarify Newton's method

--- chapt1.rkt
+++ chapt1.rkt
@@ -612,3 +612,25 @@
                1.0))
 (cube-root 27)
 
+(define (deriv g)
+  (lambda (x)
+    (/ (- (g (+ x dx)) (g x))
+       dx)))
+
+(define dx 0.00001)
+
+(define (cube x) (* x x x))
+((deriv cube) 5)
+
+(define (newton-transform g)
+  (lambda (x)
+    (- x (/ (g x) ((deriv g) x)))))
+
+(define (newtons-method g guess)
+  (fixed-point (newton-transform g) guess))
+
+(define (sqrt x)
+  (newtons-method (lambda (y) (- (square y) x))
+                  1.0))
+(sqrt 9)
+
