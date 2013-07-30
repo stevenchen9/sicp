8e0aad38d083c4b341584150c5b02d77e912adb3
Steve Shogren <steve.a.shogren@gmail.com>
2013-07-30 17:12:09 -0400

    Making a paired data structure using a list, and adding a function to 'clean up' the rational numbers when entered

--- chapt1.rkt
+++ chapt1.rkt
@@ -682,11 +682,68 @@
   (repeated (smooth f dx) n))
 ((smooth-n f 1.0 100) 5)
 
+(define (linear-combination a b x y)
+  (+ (* a x) (* b y)))
+(define (linear-combination a b x y)
+  (add (mul a x) (mul b y)))
+
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (mul-rat x y)
+  (make-rat  (* (numer x) (numer y))
+             (* (denom x) (denom y))))
+
+(define (div-rat x y)
+  (make-rat  (* (numer x) (denom y))
+             (* (denom x) (numer y))))
+
+(define (equal-rat? x y)
+  (= (* (numer x) (denom y))
+     (* (numer y) (denom x))))
+
+(define (make-rat n d) (cons n d))
+(define (numer x) (car x))
+(define (denom x) (cdr x))
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
+(define one-half (make-rat 1 2))
+(print-rat one-half)
+(define one-third (make-rat 1 3))
+(print-rat (add-rat one-half one-third))
+(print-rat (mul-rat one-half one-third))
+(print-rat (add-rat one-third one-third)) ;6/9
+
+(define (make-rat n d)
+  (let ((g (gcd n d)))
+    (cons (/ n g) (/ d g))))
 
 
+(print-rat (add-rat one-third one-third)) ;2/3
 
-
-
-
+;2.1
+(define (make-pos x) (invert-if x (negative? x)))
+(define (make-neg x) (invert-if x (positive? x)))
+(define (invert-if x pred)
+  (if pred
+      (* -1 x)
+      x))
+(define (make-rat n d)
+  (if (negative? (/ n d))
+      (cons (make-neg n) (make-pos d))
+      (cons (make-pos n) (make-pos d))))
+(make-rat -1 2) ;-1/2
+(make-rat -1 -2) ;1/2
+(make-rat 1 -2) ;-1/2
+(make-rat 1 2) ;1/2
 
 
