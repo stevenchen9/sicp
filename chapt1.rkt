4f2644ac13616a08ba558ac7c387225bbeb65346
Steve Shogren <steve.a.shogren@gmail.com>
2013-08-02 17:00:06 -0400

    Making a lambda based cons, car, and cdr

--- chapt1.rkt
+++ chapt1.rkt
@@ -818,3 +818,23 @@
                (make-point 0 1)))
 (rect-perimeter rect1) ;;4.0
 
+
+(define (cons x y)
+  (define (dispatch m)
+    (cond ((= m 0) x)
+          ((= m 1) y)
+          (else (error "argument not 0 or 1 -- CONS" m))))
+  dispatch)
+
+(define (car z) (z 0))
+(define (cdr z) (z 1))
+
+;;2.4
+(define (cons x y)
+  (lambda (m) (m x y)))
+(define (car z)
+  (z (lambda (p q) p)))
+(define (cdr z)
+  (z (lambda (p q) q)))
+(car (cons 1 2))
+
