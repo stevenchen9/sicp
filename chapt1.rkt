baeb5828462b4168b3e2face452399e97e3364a4
Steve Shogren <steve.a.shogren@gmail.com>
2013-08-23 17:09:20 -0400

    Making a function to count the ways to add up to a certain amount of currency

--- chapt1.rkt
+++ chapt1.rkt
@@ -979,10 +979,30 @@
       (append (reverse (cdr li)) (list (car li)))))
 
 (reverse (list 1 4 9 16 25))
-;; =>(25 16 9 4 1)
+;; => (25 16 9 4 1)
+
+;; 2.19
 
 
+(define (count-change amount)
+  (cc amount us-coins))
 
+(define us-coins (list 50 25 10 5 1))
+(define us-coins (list 100 50 20 10 5 2 1 0.5))
+(define (cc amount coin-values)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (no-more? coin-values)) 0)
+        (else
+         (+ (cc amount
+                (except-first-denomination coin-values))
+            (cc (- amount (first-denomination coin-values))
+                coin-values)))))
 
-
+(define (except-first-denomination li)
+  (cdr li))
+(define (no-more? li)
+  (= 0 (length li)))
+(define (first-denomination li)
+  (car li))
+(count-change 100)
 
