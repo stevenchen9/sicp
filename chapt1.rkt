d775d3c63a21a99e2637a44b4e84818e08573de7
Steve Shogren <steve.a.shogren@gmail.com>
2013-09-07 10:11:13 -0400

    Redefining the two other operations as seq flows

--- chapt1.rkt
+++ chapt1.rkt
@@ -1359,3 +1359,39 @@
 (accumulate cons null (list 1 2 3 4 5))
 ;; => (1 2 3 4 5)
 
+;; An incrementing enumerator
+(define (enumerate-interval low high)
+  (if (> low high)
+      null
+      (cons low (enumerate-interval (+ low 1) high))))
+(enumerate-interval 2 7)
+;;=> (2 3 4 5 6 7)
+
+;; An tree walking operation
+(define (enumerate-tree tree)
+  (cond ((null? tree) null)
+        ((not (pair? tree)) (list tree))
+        (else (append (enumerate-tree (car tree))
+                      (enumerate-tree (cdr tree))))))
+(enumerate-tree (list 1 (list 2 (list 3 4)) 5))
+;; => (1 2 3 4 5)
+
+;; Redefining the two earlier functions as seq operations
+(define (sum-odd-squares tree)
+  (accumulate +
+              0
+              (map square
+                   (filter odd?
+                           (enumerate-tree tree)))))
+
+(define (even-fibs n)
+  (accumulate cons
+              null
+              (filter even?
+                      (map fib
+                           (enumerate-interval 0 n)))))
+
+
+
+
+
