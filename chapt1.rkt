0c2cce003b47c79ec3410fd9d6e3ff539ea491d8
Steve Shogren <steve.a.shogren@gmail.com>
2013-09-11 19:13:27 -0400

    An intro to fold-right/left

--- chapt1.rkt
+++ chapt1.rkt
@@ -1470,10 +1470,28 @@
 (dot-product (first  m) (second  m))
 ;; => 56
 
-(define (matrix-*-vector m v)
-  (map m))
+;; 2.28
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
+(define fold-right accumulate)
 
-(matrix-*-vector m (first m))
+(fold-right / 1 (list 1 2 3))
+;; => 3/2
+(fold-left / 1 (list 1 2 3))
+;; => 1/6
+(fold-right list null (list 1 2 3))
+;; => (1 (2 (3 ()))
+(fold-left list null (list 1 2 3))
+;; => (((() 1) 2) 3)
 
-
+;; Produces the same value for any seq
+(fold-right + 0 (list 1 2 3))
+;; => 6
+(fold-left + 0 (list 1 2 3))
+;; => 6
 
