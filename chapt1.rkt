a46da5cf93ea9c76e59eaa7a3e513fa356d77efb
Steve Shogren <steve.a.shogren@gmail.com>
2013-09-13 14:12:18 -0400

    A complext example of nested maps

--- chapt1.rkt
+++ chapt1.rkt
@@ -1509,3 +1509,34 @@
 (reverse (list 1 2 3 4))
 ;; => (4 3 2 1)
 
+(define (flatmap proc seq)
+  (accumulate append null (map proc seq)))
+
+(define (prime-sum? pair)
+  (prime? (+ (car pair) (cadr pair))))
+
+(define (make-pair-sum pair)
+  (list (car pair) (cadr pair) (+ (car pair) (cadr pair))))
+
+(define (prime-sum-pairs n)
+  (map make-pair-sum
+       (filter prime-sum?
+               (flatmap
+                (lambda (i)
+                  (map (lambda (j) (list i j))
+                       (enumerate-interval 1 (- i 1))))
+                (enumerate-interval 1 n)))))
+
+(define (permutations s)
+  (if (null? s)
+      (list null)
+      (flatmap (lambda (x)
+                 (map (lambda (p) (cons x p))
+                      (permutations (remove x s))))
+               s)))
+
+(define (remove item sequence)
+  (filter (lambda (x) (not (= x item)))
+          sequence))
+
+(permutations (list 1 2 3 4))
