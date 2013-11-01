6a3a1a380ad69628c3e4b87b057720e974ff2b82
Steve Shogren <steve.a.shogren@gmail.com>
2013-11-01 17:31:32 -0400

    Copying down the homework for the binary tree converter

--- chapt1.rkt
+++ chapt1.rkt
@@ -2020,5 +2020,24 @@ functionality
                     (left-branch set)
                     (adjoin-set x (right-branch set))))))
 
+*** 2.63 Convert binary tree to list
+
+(define (tree->list-1 tree)
+  (if (null? tree)
+      '()
+      (append (tree->list-1 (left-branch tree))
+              (cons (entry tree)
+                    (tree->list-1 (right-branch tree))))))
+
+(define (tree->list-2 tree)
+  (define (copy-to-list tree result-list)
+    (if (null? tree)
+        result-list
+        (copy-to-list (left-branch tree)
+                      (cons (entry tree)
+                            (copy-to-list (right-branch tree)
+                                          result-list)))))
+  (copy-to-list tree '()))
+
 
 
