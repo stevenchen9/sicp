a73116463d15e8b67e56af512f7e3293bd75a78f
Steve Shogren <steve.a.shogren@gmail.com>
2013-11-28 19:43:21 -0500

    Adding the code to make a huffman tree

--- chapt1.rkt
+++ chapt1.rkt
@@ -2222,3 +2222,20 @@ functionality
 
 (lookup 7 x)
 
+** 2.3.4 Example: Huffman Encoding Trees
+
+(define (make-leaf symbol weight)
+  (list 'leaf symbol weight))
+(define (leaf? object)
+  (eq? (car object) 'leaf))
+(define (symbol-leaf x) (cadr x))
+(define (weight-leaf x) (caddr x))
+
+(define (make-code-tree left right)
+  (list left
+        right
+        (append (symbols left) (symbols right))
+        (+ (weight left) (weight right))))
+
+(define (left-branch tree) (car tree))
+(define (right-branch tree) (cadr tree))
