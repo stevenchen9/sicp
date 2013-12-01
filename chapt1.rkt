c7125d0a794f2be7f5cf06a18c9f5fc43ae4551e
Steve Shogren <steve.a.shogren@gmail.com>
2013-12-01 18:52:35 -0500

    Working on the encoding of a hufman tree

--- chapt1.rkt
+++ chapt1.rkt
@@ -2288,5 +2288,33 @@ functionality
                    (make-code-tree (make-leaf 'D 1)
                                    (make-leaf 'C 1)))))
 
+;; => ((leaf A 4) ((leaf B 2) ((leaf D 1) (leaf C 1) (D C) 2) (B D C) 4) (A B D C) 8)
+
 (define sample-message '(0 1 1 0 0 1 0 1 0 1 1 1 0))
 
+(decode sample-message sample-tree)
+;; => (A D A B B C A) 
+
+*** 2.68
+
+(define (encode message tree)
+  (if (null? message)
+      '()
+      (append (encode-symbol (car message) tree)
+              (encode (cdr message) tree))))
+
+(define (decode bits tree)
+  (define (decode-1 bits current-branch)
+    (if (null? bits)
+        '()
+        (let ((next-branch
+               (choose-branch (car bits) current-branch)))
+          (if (leaf? next-branch)
+              (cons (symbol-leaf next-branch)
+                    (decode-1 (cdr bits) tree))
+              (decode-1 (cdr bits) next-branch)))))
+  (decode-1 bits tree))
+(define (choose-branch bit branch)
+  (cond ((= bit 0) (left-branch branch))
+        ((= bit 1) (right-branch branch))
+        (else (error "bad bit -- CHOOSE-BRANCH" bit))))
