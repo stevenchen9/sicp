27e9f70f08b3fb071044fe806428639065943546
Steve Shogren <steve.a.shogren@gmail.com>
2013-12-08 15:27:19 -0500

    Working on showing the data in a normal format with something akin to pattern matching

--- 2.4.rkt
+++ 2.4.rkt
@@ -49,6 +49,32 @@
 (define (make-from-mag-ang-polar r a)
   (attach-tag 'polar (cons r a)))
 
+;; polymorphic selectors for the data
+(define (real-part z)
+  (cond ((rectangular? z)
+         (real-part-rectangular (contents z)))
+        ((polar? z)
+         (real-part-polar (contents z)))
+        (else (error "Unknown type -- REAL-PART" z))))
+(define (imag-part z)
+  (cond ((rectangular? z)
+         (imag-part-rectangular (contents z)))
+        ((polar? z)
+         (imag-part-polar (contents z)))
+        (else (error "Unknown type -- IMAG-PART" z))))
+(define (magnitude z)
+  (cond ((rectangular? z)
+         (magnitude-rectangular (contents z)))
+        ((polar? z)
+         (magnitude-polar (contents z)))
+        (else (error "Unknown type -- MAGNITUDE" z))))
+(define (angle z)
+  (cond ((rectangular? z)
+         (angle-rectangular (contents z)))
+        ((polar? z)
+         (angle-polar (contents z)))
+        (else (error "Unknown type -- ANGLE" z))))
+
 ;; The way our add/sub/mul/div functions work
 ;; either data representation will function
 
@@ -69,3 +95,5 @@
 (define (polar? z)
   (eq? (type-tag z) 'polar))
 
+(define (add-complex z1 z2)
+  (make-from-real-imag (+ )))
