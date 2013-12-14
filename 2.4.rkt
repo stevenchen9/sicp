10776a87178fedb093a5b105b520919850e20839
Steve Shogren <steve.a.shogren@gmail.com>
2013-12-14 17:54:41 -0500

    Making generic selectors

--- 2.4.rkt
+++ 2.4.rkt
@@ -173,3 +173,29 @@
        (lambda (r a) (tag (make-from-mag-ang r a))))
   'done)
 
+;; To now map our selectors to the table:
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+          (apply proc (map contents args))
+          (error
+           "No method for these types -- APPLY-GENERIC"
+           (list op type-tags))))))
+ ;; why is type-tags a list at all? why not a single element?
+
+;; generic selectors!
+(define (real-part z) (apply-generic 'real-part z))
+(define (imag-part z) (apply-generic 'imag-part z))
+(define (magnitude z) (apply-generic 'magnitude z))
+(define (angle z) (apply-generic 'angle z))
+
+;; now the entire stack is unchanged if a new
+;; type-package is added to the system!
+
+;; We can also pick a representation for the
+;; default "constructor"
+(define (make-from-real-imag x y)
+  ((get 'make-from-real-imag 'rectangular) x y))
+(define (make-from-mag-ang r a)
+  ((get 'make-from-mag-ang 'polar) r a))
