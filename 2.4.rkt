32447e15ad5040eb484ea7a51c0178117205bf0f
Steve Shogren <steve.a.shogren@gmail.com>
2013-12-23 17:35:13 -0500

    An example of an apply-generic method that would use a coercion scheme

--- 2.4.rkt
+++ 2.4.rkt
@@ -439,3 +439,31 @@
   (cond ((pair? datum) (cdr datum))
         ((number? datum) datum)
         (error "Bad tagged datum -- CONTENTS" datum)))
+
+;; "Coercion scheme" aware apply-generic
+;; allows types to be converted using a lookup from one type
+;; to another, but then that means you might need at most
+;; n^2 converters for n types
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+          (apply proc (map contents args))
+          (if (= (length args) 2)
+              (let ((type1 (car type-tags))
+                    (type2 (cadr type-tags))
+                    (a1 (car args))
+                    (a2 (cadr args)))
+                (let ((t1->t2 (get-coercion type1 type2))
+                      (t2->t1 (get-coercion type2 type1)))
+                       (cond (t1->t2
+                              (apply-generic op (t1->t2 a1) a2))
+                             (t2->t1
+                              (apply-generic op a1 (t2->t1 a2)))
+                             (else
+                              (error "No method for these types"
+                                     (list op type-tags))))))
+              (error "No method for these types"
+                     (list op type-tags)))))))
+
+
