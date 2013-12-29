7e4a079b3eff7cea3bb8f196d6c8cffcc08ed986
Steve Shogren <steve.a.shogren@gmail.com>
2013-12-29 15:37:49 -0500

    A polynomial math function set

--- 2.4.rkt
+++ 2.4.rkt
@@ -627,3 +627,19 @@
 ;; (higher? 'rational 'integer tower-of-types) => #t
 ;; (higher? 'integer 'real tower-of-types) => #f
 
+
+;; 2.5.1
+(define (add-poly p1 p2)
+  (if (same-variable? (variable p1) (variable p2))
+      (make-poly (variable p1)
+                 (add-terms (term-list p1)
+                            (term-list p2)))
+      (error "Polys not in same var -- ADD-POLY"
+             (list p1 p2))))
+(define (mul-poly p1 p2)
+  (if (same-variable? (variable p1) (variable p2))
+      (make-poly (variable p1)
+                 (mul-terms (term-list p1)
+                            (term-list p2)))
+      (error "Polys not in same var -- MUL-POLY"
+             (list p1 p2))))
