bddb08be84dc012641e6462f1b1a2885df6de3b4
Steve Shogren <steve.a.shogren@gmail.com>
2013-12-30 17:41:12 -0500

    Additional poly math functions

--- 2.4.rkt
+++ 2.4.rkt
@@ -643,3 +643,36 @@
                             (term-list p2)))
       (error "Polys not in same var -- MUL-POLY"
              (list p1 p2))))
+
+(define (install-polynomial-package)
+  ;;internal procs
+  ;;representation of poly
+  (define (make-poly variable term-list)
+    (cons variable term-list))
+  (define (variable p) (car p))
+  (define (term-list p) (cdr p))
+  (define (tag p) (attach-tag 'polynomial p))
+  (put 'add '(polynomial polynomial)
+       (lambda (p1 p2) (tag (add-poly p1 p2))))
+  (put 'mul '(polynomial polynomial)
+       (lambda (p1 p2) (tag (mul-poly p1 p2))))
+  (put 'make 'polynomial
+       (lambda (var terms) (tag (make-poly var terms))))
+  'done)
+
+(define (add-terms L1 L2)
+  (cond ((empty-termlist? L1) L2)
+        ((empty-termlist? L2) L1)
+        (else
+         (let ((t1 (first-term L1)) (t2 (first-term L2)))
+           (cond ((> (order t1) (order t2))
+                  (adjoin-term
+                   t1 (add-terms (rest-terms L1) L2)))
+                 ((< (order t1) (order t2))
+                  (adjoin-term
+                   t2 (add-terms L1 (rest-terms L2))))
+                 (else
+                  (adjoin-term (order t1)
+                               (add (coeff t1) (coeff t2)))
+                  (add-terms (rest-terms L1)
+                             (rest-terms L2))))))))
