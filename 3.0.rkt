0792ccab7170a40398a4986dbf54272b99ba6bd0
Steve Shogren <steve.a.shogren@gmail.com>
2014-01-04 19:13:14 -0500

    A stateful processor that returns a closure that uses mutation to change its internal state

--- 3.0.rkt
+++ 3.0.rkt
@@ -25,3 +25,34 @@
 ;; => "Insufficient funds"
 (withdraw 15)
 ;; => 35
+
+;; Rewriting withdraw to encapsulate the balance
+(define new-withdraw
+  (let ((balance 100))
+    (lambda (amount)
+      (if (>= balance amount)
+          (begin (set! balance (- balance amount))
+                 balance)
+          "Insufficient funds"))))
+
+(new-withdraw 55)
+;; => 45
+
+;; A withdraw that returns a "stateful processor"
+(define (make-withdraw balance)
+  (lambda (amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+               balance)
+        "Insufficient funds")))
+  
+(define W1 (make-withdraw 100))
+(define W2 (make-withdraw 100))
+(W1 50)
+;; => 50
+(W2 70)
+;; => 30
+(W2 40)
+;; => "Insufficient funds"
+(W1 40)
+;; => 10
