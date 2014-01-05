af6fe2781777e6ed823f781a4e67eb72f8008ed2
Steve Shogren <steve.a.shogren@gmail.com>
2014-01-05 18:44:27 -0500

    A message passing style closure object

--- 3.0.rkt
+++ 3.0.rkt
@@ -56,3 +56,32 @@
 ;; => "Insufficient funds"
 (W1 40)
 ;; => 10
+
+;; A message passing style stateful closure that allows for a
+;; dispatch to fully encapsulate and allow for external
+;; mutation of its state 
+(define (make-account balance)
+  (define (withdraw amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+               balance)
+        "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (define (dispatch m)
+    (cond ((eq? m 'withdraw) withdraw)
+          ((eq? m 'deposit) deposit)
+          (else (error "Unknown request -- MAKE-ACCOUNT"
+                       m))))
+  dispatch)
+
+(define acc (make-account 100))
+((acc 'withdraw) 50)
+;; => 50
+((acc 'withdraw) 60)
+;; => "Insufficient funds"
+((acc 'deposit) 40)
+;; => 90
+((acc 'withdraw) 60)
+;; => 30
