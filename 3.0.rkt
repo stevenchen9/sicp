2e88b86f0bdf374710052161c28d3e8d2d471488
Steve Shogren <steve.a.shogren@gmail.com>
2014-01-16 17:09:51 -0500

    Showing the frames for two differnt fact versions

--- 3.0.rkt
+++ 3.0.rkt
@@ -328,4 +328,32 @@
 (+ (f 0) (f 1)) ;; => 1  
 (+ (f 1) (f 0)) ;;=> 0
 
+;; 3.9
+(define (factorial n)
+  (if (= n 1)
+      1
+      (* n (factorial (- n 1)))))
+(factorial 6)
+;; E1 n = 6
+;; E2 n = 5
+;; E3 n = 4
+;; E4 n = 3
+;; E5 n = 2
+;; E6 n = 1
 
+(define (factorial n)
+  (fact-iter 1 1 n))
+
+(define (fact-iter product counter max-count)
+  (if (> counter max-count)
+      product
+      (fact-iter (* counter product)
+                 (+ counter 1)
+                 max-count)))
+;; (factorial 6) E1 n = 6
+;; E2 product = 1, counter = 1, max-count = 6
+;; E3 product = 1, counter = 2, max-count = 6
+;; E4 product = 2, counter = 3, max-count = 6
+;; E5 product = 6, counter = 4, max-count = 6
+;; E6 product = 24, counter = 5, max-count = 6
+;; E7 product = 120, counter = 6, max-count = 6
