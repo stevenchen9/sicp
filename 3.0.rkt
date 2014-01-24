fc50c78660a9e7fc4c2e58b5d3b0f0e3222d0055
Steve Shogren <steve.a.shogren@gmail.com>
2014-01-24 14:54:42 -0500

    Exercise 3.12

--- 3.0.rkt
+++ 3.0.rkt
@@ -386,3 +386,47 @@
 (sqrt 9)
 
 
+
+;; Mutable Cons cells
+
+(define (cons x y)
+  (let ((new (get-new-pair)))
+    (set-car! new x)
+    (set-cdr! new y)
+    new))
+
+;; 3.12
+
+;; old immutable append 
+(define (append x y)
+  (if (null? x)
+      y
+      (cons (car x) (append (cdr x) y))))
+
+;; new mutable append
+(define (append! x y)
+  (set-cdr! (last-pair x) y)
+  x)
+
+(define (last-pair x)
+  (if (null? (cdr x))
+      x
+      (last-pair (cdr x))))
+
+(define x (list 'a 'b))
+(define y (list 'c 'd))
+(define z (append x y))
+z
+;; (a b c d)
+(cdr x)
+;; b
+(define w (append! x y))
+w
+;; (a b c d)
+(cdr x)
+;; (b c d)
+
+;; 3.13
+(define (make-cycle x)
+  (set-cdr! (last-pair x) x)
+  x)
