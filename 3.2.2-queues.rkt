7a271106010a5b2995ba1384b23a078099c4274c
Steve Shogren <steve.a.shogren@gmail.com>
2014-02-12 17:03:28 -0500

    Two or-gate implementations

--- 3.2.2-queues.rkt
+++ 3.2.2-queues.rkt
@@ -381,3 +381,53 @@
     (half-adder a s sum c2)
     (or-gate c1 c2 c-out)
     'ok))
+
+(define (inverter input output)
+  (define (invert-input)
+    (let ((new-value (logical-not (get-signal input))))
+      (after-delay inverter-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! input inverter-input)
+  'ok)
+
+(define (logical-not s)
+  (cond ((= s 0) 1)
+        ((= s 1) 0)
+        (else (error "Invalid signal" s))))
+
+(define (and-gate a1 a2 output)
+  (define (and-action-procedure)
+    (let ((new-value
+           (logical-and (get-signal a1) (get-signal a2))))
+      (after-delay and-gate-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! a1 and-action-procedure)
+  (add-action! a2 and-action-procedure)
+  'ok)
+
+;; 3.28
+(define (or-gate a1 a2 output)
+  (define (or-action-procedure)
+    (let ((new-value
+           (logical-or (get-signal a1) (get-signal a2))))
+      (after-delay or-gate-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! a1 or-action-procedure)
+  (add-action! a2 or-action-procedure)
+  'ok)
+
+;; 3.29
+;; or with "and" and "inverter"
+;; the time delay for this will be the same as it is for
+;; just the inverter then the and, same as it would be
+;; in the regular circuit
+(define (or-gate a1 a2 output)
+  (let ((inv-to-and-wire (make-wire)))
+    (inverter a1 inv-to-and-wire)
+    (and-gate a1 inv-to-and-wire output)
+    'ok))
+
+
