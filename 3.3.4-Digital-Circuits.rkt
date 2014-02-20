9f5f00419a93768265e12e926f0e927d52d0937b
Steve Shogren <steve.a.shogren@gmail.com>
2014-02-19 22:04:24 -0500

    Finishing the curcuit example

--- 3.3.4-Digital-Circuits.rkt
+++ 3.3.4-Digital-Circuits.rkt
@@ -190,7 +190,7 @@
 (define (set-segments! agenda segments)
   (set-cdr! agenda segments))
 (define (first-segment agenda) (car (segments agenda)))
-(define (rest-segment agenda) (cdr (segments agenda)))
+(define (rest-segments agenda) (cdr (segments agenda)))
 (define (empty-agenda? agenda)
   (null? (segments agenda)))
 
@@ -220,5 +220,21 @@
          (cons (make-time-segment time action)
                segments))
         (add-to-segments! segments))))
+
+(define (remove-first-agenda-item! agenda)
+  (let ((q (segment-queue (first-segment agenda))))
+    (delete-queue! q)
+    (if (empty-agenda? q)
+        (set-segments! agenda (rest-segments agenda))
+        't)))
+
+(define (first-agenda-item agenda)
+  (if (empty-agenda? agenda)
+      (error "Agenda is empty -- FIRST-AGENDA-ITEM")
+      (let ((first-seg (first-segment agenda)))
+        (set-current_time! agenda (segment-time first-seg))
+        (front-queue (segment-queue first-seg)))))
+
+
 
 
