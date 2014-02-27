7533add8dd96bad2b256a2e65e1643e37d14535c
Steve Shogren <steve.a.shogren@gmail.com>
2014-02-27 16:56:49 -0500

    Analysis of another form of make-account with an internal serializer

--- 3.4.2-Mechanisms-for-controlling-concurrency.rkt
+++ 3.4.2-Mechanisms-for-controlling-concurrency.rkt
@@ -70,4 +70,26 @@
 ;; happening at the same time, before the (set!) occurs
 
 ;; 3.42
-
+;; Ben's new make-account with a single serialized function
+(define (make-account balance)
+  (define (withdraw amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+               balance)
+        "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (let ((protected (make-serializer)))
+    (let ((protected-withdraw (protected withdraw))
+          (protected-deposit (protected deposit)))
+      (define (dispatch m)
+        (cond ((eq? m 'withdraw) protected-withdraw)
+              ((eq? m 'deposit) protected-deposit)
+              ((eq? m 'balance) balance)
+              (else (error "Unknown request -- MAKE-ACCOUNT"
+                           m)))))
+    dispatch))
+;; This is a safe change to make, even the balance will never
+;; be in an "intermediate" state, it is either before or after,
+;; no invalid between
