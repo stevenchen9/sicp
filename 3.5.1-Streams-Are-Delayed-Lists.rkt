40fcbfe511c01c32f476c5b0b72035b0d9f3f337
Steve Shogren <steve.a.shogren@gmail.com>
2014-03-06 17:06:59 -0500

    Analysis of using the memoizer vs delay

--- 3.5.1-Streams-Are-Delayed-Lists.rkt
+++ 3.5.1-Streams-Are-Delayed-Lists.rkt
@@ -116,6 +116,7 @@
 (memo-proc (lambda () <EXP>))
 ;; and `force` can stay the same
 (define the-empty-stream '())
+
 ;; 3.50 - Generalized stream-map
 (define (stream-map proc . argstreams)
   (if (stream-null? (car argstreams))
@@ -139,3 +140,21 @@
 ;; => 6 7
 
 ;; 3.52
+(define sum 0)
+(define (accum x)
+  (set! sum (+ x sum))
+  sum)
+
+(define seq (stream-map accum (stream-enumerate-interval 1 20)))
+(define y (stream-filter even? seq))
+;; => sum = 6
+(define z (stream-filter (lambda (x) (= (remainder x 5) 0))
+                         seq))
+;; => sum = 10 
+
+(stream-ref y 7)
+;; => sum = 136 
+(display-stream z)
+;; => sum = 210
+;; => done 10 15 45 55 105 120 190 210
+
