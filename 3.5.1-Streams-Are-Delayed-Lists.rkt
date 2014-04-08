0fd39fcd2886a353917bb13d6b1bd05e4790bc1a
Steve Shogren <steve.a.shogren@gmail.com>
2014-04-08 06:53:55 -0400

    Using force/delay to make a different version of the integral stream

--- 3.5.1-Streams-Are-Delayed-Lists.rkt
+++ 3.5.1-Streams-Are-Delayed-Lists.rkt
@@ -555,8 +555,42 @@
                                        int)))))
   int)
 
-
+;; This uses delay-force to ensure the initial value of the
+;; dy is set, which needs the first value of y, but that starts
+;; at initial value
 (define (solve f y0 dt)
   (define y (integral (delay dy) y0 dt))
   (define dy (stream-map f y))
   y)
+(stream-ref (solve (lambda (y) y) 1 0.001) 1000)
+;; => 2.716924
+
+
+;;   *Exercise 3.77:* The `integral' procedure used above was analogous
+;;   to the "implicit" definition of the infinite stream of integers in
+;;   section *Note 3-5-2::.  Alternatively, we can give a definition of
+;;   `integral' that is more like `integers-starting-from' (also in
+;;   section *Note 3-5-2::):
+
+;;        (define (integral integrand initial-value dt)
+;;          (cons-stream initial-value
+;;                       (if (stream-null? integrand)
+;;                           the-empty-stream
+;;                           (integral (stream-cdr integrand)
+;;                                     (+ (* dt (stream-car integrand))
+;;                                        initial-value)
+;;                                     dt))))
+
+;;   When used in systems with loops, this procedure has the same
+;;   problem as does our original version of `integral'.  Modify the
+;;   procedure so that it expects the `integrand' as a delayed argument
+;;   and hence can be used in the `solve' procedure shown above.
+(define (integral delayed-integrand initial-value dt)
+  (cons initial-value
+        (delay (let ((integrand (force delayed-integrand)))
+                 (if (stream-null? integrand)
+                     the-empty-stream
+                     (integral (stream-cdr integrand)
+                               (+ (* dt (stream-car integrand))
+                                  initial-value)
+                               dt))))))
