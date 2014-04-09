2fabd3e57f2e2098bf658c7c8db32f29245d1019
Steve Shogren <steve.a.shogren@gmail.com>
2014-04-09 06:47:37 -0400

    A list of implementations to the language interface, specifically for scheme

--- 4.1-MetacircularEvaluator.rkt
+++ 4.1-MetacircularEvaluator.rkt
@@ -88,3 +88,43 @@
       (let* ((rest (eval (rest-operand exps) env))
              (first (eval (first-operands exps) env)))
         (cons left rest))))
+
+
+;; The "language specific" representation of the MC interfaces
+(define (self-evaluating? exp)
+  (cond ((number? exp) true)
+        ((string? exp) true)
+        (else false)))
+(define (variable? exp) (symbol? exp))
+(define (quoted? exp)
+  (tagged-list? exp 'quote))
+
+(define (text-of-quotation exp) (cadr exp))
+(define (tagged-list? exp tag)
+  (if (pair? exp)
+      (eq? (car exp) tag)
+      false))
+(define (assignment? exp)
+  (tagged-list? exp 'set!))
+
+(define (assignment-variable exp) (cadr exp))
+
+(define (assignment-value exp) (caddr exp))
+(define (definition? exp)
+  (tagged-list? exp 'define))
+
+(define (definition-variable exp)
+  (if (symbol? (cadr exp))
+      (cadr exp)
+      (caadr exp)))
+
+(define (definition-value exp)
+  (if (symbol? (cadr exp))
+      (caddr exp)
+      (make-lambda (cdadr exp)   ; formal parameters
+                   (cddr exp)))) ; body
+(define (lambda? exp) (tagged-list? exp 'lambda))
+
+(define (lambda-parameters exp) (cadr exp))
+
+(define (lambda-body exp) (cddr exp))
