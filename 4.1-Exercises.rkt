24e0b180a92c439af86324ee729606cc63271357
Steve Shogren <steve.a.shogren@gmail.com>
2014-04-11 07:13:27 -0400

    A first pass at let*, but only allowing a single expression in the body, still mising a way to 'flatten' the list of body elements

--- 4.1-Exercises.rkt
+++ 4.1-Exercises.rkt
@@ -221,3 +221,42 @@
         (map cadr (bindings exp))))
 (let->combination '(let ((x 1) (y 2)) (+ x y)))
 ;; => ((lambda (x y) ((+ x y))) 1 2)
+
+
+;;   *Exercise 4.7:* `Let*' is similar to `let', except that the
+;;   bindings of the `let' variables are performed sequentially from
+;;   left to right, and each binding is made in an environment in which
+;;   all of the preceding bindings are visible.  For example
+
+;;        (let* ((x 3)
+;;               (y (+ x 2))
+;;               (z (+ x y 5)))
+;;          (* x z))
+
+;;   returns 39.  Explain how a `let*' expression can be rewritten as a
+;;   set of nested `let' expressions, and write a procedure
+;;   `let*->nested-lets' that performs this transformation.  If we have
+;;   already implemented `let' (*Note Exercise 4-6::) and we want to
+;;   extend the evaluator to handle `let*', is it sufficient to add a
+;;   clause to `eval' whose action is
+
+;;        (eval (let*->nested-lets exp) env)
+
+;;   or must we explicitly expand `let*' in terms of non-derived
+;;   expressions?
+
+(define (let*? exp) (tagged-list? exp 'let*))
+(define (bindings exp) (cadr exp))
+(define (body exp) (cddr exp))
+(define (let*->combination exp)
+  (define (i-let bindings)
+    (if (empty? bindings)
+        (car (body exp))
+        (cons (list 'lambda
+                    (list (caar bindings))
+                    (i-let (cdr bindings)))
+              (cadar bindings))))
+  (i-let (bindings exp)))
+(let*->combination '(let ((x 1) (y 2)) (+ x y)))
+;; => ((lambda (x) ((lambda (y) (+ x y)) . 2)) . 1)
+
