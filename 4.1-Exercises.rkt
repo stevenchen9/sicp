2969b5b4ffe53d9b9bc331ea08b80119dd016509
Steve Shogren <steve.a.shogren@gmail.com>
2014-04-16 07:14:53 -0400

    Working on changing the representation of the env for our eval

--- 4.1-Exercises.rkt
+++ 4.1-Exercises.rkt
@@ -345,3 +345,83 @@
 
 
 ;; See Alt-Lang.rkt
+
+;;   *Exercise 4.11:* Instead of representing a frame as a pair of
+;;   lists, we can represent a frame as a list of bindings, where each
+;;   binding is a name-value pair.  Rewrite the environment operations
+;;   to use this alternative representation.
+
+;; Functions for dealing with the env
+(define (enclosing-environment env) (cdr env))
+(define (first-frame env) (car env))
+(define the-empty-environment '())
+
+;; Frame => (<vars> <values>)
+(define (make-frame variables values)
+  (map (lambda (var val) (cons var val)) variables values))
+;;(define t (make-frame '(a b c) '(1 2 3)))
+
+(define (frame-variables frame)
+  (map car frame))
+;;(frame-variables t)
+
+(define (frame-values frame)
+  (map cdr frame))
+;; (frame-values t)
+
+
+;; (require scheme/mpair)
+(require (planet neil/sicp:1:17))
+
+(define (add-binding-to-frame! var val frame)
+  (set-car! frame (cons var (car frame)))
+  (set-cdr! frame (cons val (cdr frame))))
+
+(define (extend-environment vars vals base-env)
+  (if (= (length vars) (length vals))
+      (cons (make-frame vars vals) base-env)
+      (if (< (length vars) (length vals))
+          (error "Too many arguments supplied" vars vals)
+          (error "Too few arguments supplied" vars vals))))
+
+(define (lookup-variable-value var env)
+  (define (env-loop env)
+    (define (scan vars vals)
+      (cond [(null? vars)
+             (env-loop (enclosing-environment env))]
+            [(eq? var (car vars))
+             (car vals)]
+            [else (scan (cdr vars) (cdr vals))]))
+    (if (eq? env the-empty-environment)
+        (error "Unbound variable" var)
+        (let [(frame (first-frame env))]
+          (scan (frame-variables frame)
+                (frame-values frame)))))
+  (env-loop env))
+
+
+(define (set-variable-value! var val env)
+  (define (env-loop env)
+    (define (scan vars vals)
+      (cond [(null? vars)
+             (env-loop (enclosing-environment env))]
+            [(eq? var (car vars))
+             (set-car! vals val)]
+            [else (scan (cdr vars) (cdr vals))]))
+    (if (eq? env the-empty-environment)
+        (error "Unbound variable -- SET!" var)
+        (let [(frame (first-frame env))]
+          (scan (frame-variables frame)
+                (frame-values frame)))))
+  (env-loop env))
+
+(define (define-variable! var val env)
+  (let ((frame (first-frame env)))
+    (define (scan vars vals)
+      (cond [(null? vars)
+             (add-binding-to-frame! var val frame)]
+            [(eq? var (car vars))
+             (set-car! vals val)]
+            [else (scan (cdr vars) (cdr vals))]))
+    (scan (frame-variables frame)
+          (frame-values frame))))
