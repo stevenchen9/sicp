efec70b77d4e348085369eb4113dd47fe50609a2
Steve Shogren <steve.a.shogren@gmail.com>
2014-04-24 07:11:26 -0400

    Adding an explaination for why the halting problem exists

--- 4.1-Exercises.rkt
+++ 4.1-Exercises.rkt
@@ -523,3 +523,34 @@
 ;; I have it replacing the removed binding with null, as a shortcut,
 ;; but this could be replaced with a true remove for space concerns
 ;; if needed.
+
+;;   *Exercise 4.15:* Given a one-argument procedure `p' and an object
+;;   `a', `p' is said to "halt" on `a' if evaluating the expression `(p
+;;   a)' returns a value (as opposed to terminating with an error
+;;   message or running forever).  Show that it is impossible to write
+;;   a procedure `halts?' that correctly determines whether `p' halts
+;;   on `a' for any procedure `p' and object `a'.  Use the following
+;;   reasoning: If you had such a procedure `halts?', you could
+;;   implement the following program:
+
+;;        (define (run-forever) (run-forever))
+
+;;        (define (try p)
+;;          (if (halts? p p)
+;;              (run-forever)
+;;              'halted))
+
+;;   Now consider evaluating the expression `(try try)' and show that
+;;   any possible outcome (either halting or running forever) violates
+;;   the intended behavior of `halts?'.(5)
+
+;; (try try) the argument passed to (try) will be a procedure,
+;; evaluating it will always return a value.
+
+;; It is impossible to know if p is still running or running forever
+;; without knowing intimately what p is and does, which would be
+;; programmatically impossible. The best case would be to wait for a stack
+;; overflow, then use that as a general "you used too many stack frames",
+;; but this would still not help anything tail-recursive, which would
+;; simply run out of memory.
+
