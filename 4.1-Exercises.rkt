67234c1a3233406f76e2aff22a6ecf821db956ac
Steve Shogren <steve.a.shogren@gmail.com>
2014-05-09 07:10:29 -0400

    Answer for why we cannot use arbitrary nested lets for bindings

--- 4.1-Exercises.rkt
+++ 4.1-Exercises.rkt
@@ -620,3 +620,31 @@
                      (+ a b)))
 ;; => {let {{a *unassigned*} {b *unassigned*}} {begin {set! a {a-body}} {set! b {b-body}} {+ 1 2} {+ a b}}}
 
+;;   *Exercise 4.18:* Consider an alternative strategy for scanning out
+;;   definitions that translates the example in the text to
+
+;;        (lambda <VARS>
+;;          (let ((u '*unassigned*)
+;;                (v '*unassigned*))
+;;            (let ((a <E1>)
+;;                  (b <E2>))
+;;              (set! u a)
+;;              (set! v b))
+;;            <E3>))
+
+;;   Here `a' and `b' are meant to represent new variable names, created
+;;   by the interpreter, that do not appear in the user's program.
+;;   Consider the `solve' procedure from section *Note 3-5-4:::
+
+;;        (define (solve f y0 dt)
+;;          (define y (integral (delay dy) y0 dt))
+;;          (define dy (stream-map f y))
+;;          y)
+
+;;   Will this procedure work if internal definitions are scanned out
+;;   as shown in this exercise?  What if they are scanned out as shown
+;;   in the text?  Explain.
+
+;; In this exercise, one might accidentally use a symbol otherwise in
+;; use by the program. Now E3 has a new definition for a and b, causing
+;; errors.
