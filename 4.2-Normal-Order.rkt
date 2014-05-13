4a2fe33b92ef78c0ca83f6b334368907e46ee80e
Steve Shogren <steve.a.shogren@gmail.com>
2014-05-13 07:06:15 -0400

    An unless alternative that uses lambda to delay

--- 4.2-Normal-Order.rkt
+++ 4.2-Normal-Order.rkt
@@ -13,3 +13,27 @@
 ;; Yes, it should, because it will stop when n = 1
 
 
+;;   *Exercise 4.26:* Ben Bitdiddle and Alyssa P. Hacker disagree over
+;;   the importance of lazy evaluation for implementing things such as
+;;   `unless'.  Ben points out that it's possible to implement `unless'
+;;   in applicative order as a special form.  Alyssa counters that, if
+;;   one did that, `unless' would be merely syntax, not a procedure
+;;   that could be used in conjunction with higher-order procedures.
+;;   Fill in the details on both sides of the argument.  Show how to
+;;   implement `unless' as a derived expression (like `cond' or `let'),
+;;   and give an example of a situation where it might be useful to
+;;   have `unless' available as a procedure, rather than as a special
+;;   form.
+
+;; If we make unless a special form, we cannot use it with list comprehensions,
+;; or pass it in to other functions, or return it from functions. It no longer
+;; would be "first class".
+
+;; We could make it like cond or let by wrapping functions in lambdas.
+
+(define (unless1 cond left right)
+  (if cond
+      (right)
+      (left)))
+
+(unless1 true (lambda () 1) (lambda () 2))
