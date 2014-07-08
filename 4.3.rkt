863c54f52b8fb2f9f6fb4171589edc770951f003
Steve Shogren <steve.a.shogren@gmail.com>
2014-07-08 07:04:42 -0400

    A modification of the the multiple dwelling that uses lets to reduce the back tracking

--- 4.3.rkt
+++ 4.3.rkt
@@ -44,3 +44,64 @@
         (let ((k (sqrt ksq)))
           (require (integer? k))
           (list i j k))))))
+
+;;   *Exercise 4.38:* Modify the multiple-dwelling procedure to omit
+;;   the requirement that Smith and Fletcher do not live on adjacent
+;;   floors.  How many solutions are there to this modified puzzle?
+
+(define (multiple-dwelling)
+  (let ((baker (amb 1 2 3 4 5))
+        (cooper (amb 1 2 3 4 5))
+        (fletcher (amb 1 2 3 4 5))
+        (miller (amb 1 2 3 4 5))
+        (smith (amb 1 2 3 4 5)))
+    (require
+     (distinct? (list baker cooper fletcher miller smith)))
+    (require (not (= baker 5)))
+    (require (not (= cooper 1)))
+    (require (not (= fletcher 5)))
+    (require (not (= fletcher 1)))
+    (require (> miller cooper))
+    ;; (require (not (= (abs (- smith fletcher)) 1)))
+    (require (not (= (abs (- fletcher cooper)) 1)))
+    (list (list 'baker baker)
+          (list 'cooper cooper)
+          (list 'fletcher fletcher)
+          (list 'miller miller)
+          (list 'smith smith))))
+
+;;   *Exercise 4.40:* In the multiple dwelling problem, how many sets
+;;   of assignments are there of people to floors, both before and
+;;   after the requirement that floor assignments be distinct?  It is
+;;   very inefficient to generate all possible assignments of people to
+;;   floors and then leave it to backtracking to eliminate them.  For
+;;   example, most of the restrictions depend on only one or two of the
+;;   person-floor variables, and can thus be imposed before floors have
+;;   been selected for all the people.  Write and demonstrate a much
+;;   more efficient nondeterministic procedure that solves this problem
+;;   based upon generating only those possibilities that are not already
+;;   ruled out by previous restrictions.  (Hint: This will require a
+;;   nest of `let' expressions.)
+
+(define (the-lower-of a b)
+  (if (< a b) a b))
+(define (the-higher-of a b)
+  (if (> a b) a b))
+
+(define (multiple-dwelling)
+  (let ((baker (amb 1 2 3 4))
+        (cooper (amb 2 3 4 5)))
+    (let ((miller (an-integer-between cooper 5)))
+      (let ((fletcher (an-integer-between
+                       (the-lower-of (+ 1 cooper) 4)
+                       (the-higher-of (- 1 cooper) 2))))
+        (let ((smith (an-integer-between (+ 1 fletcher) (- 1 fletcher))))
+          (require
+           (distinct? (list baker cooper fletcher miller smith)))
+          (require (not (= (abs (- smith fletcher)) 1)))
+          (require (not (= (abs (- fletcher cooper)) 1)))
+          (list (list 'baker baker)
+                (list 'cooper cooper)
+                (list 'fletcher fletcher)
+                (list 'miller miller)
+                (list 'smith smith)))))))
