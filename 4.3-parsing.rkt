47a5cb419654975c2a6574290bf10955164591f6
Steve Shogren <steve.a.shogren@gmail.com>
2014-08-01 06:55:21 -0400

    Adding the rest of the sentence parsers

--- 4.3-parsing.rkt
+++ 4.3-parsing.rkt
@@ -1,4 +1,4 @@
-
+;; #lang swindle
 ;; Parsing sentences
 (define nouns '(noun student professor cat class))
 
@@ -24,15 +24,54 @@
     (list (car word-list) found-word)))
 
 
-     (define *unparsed* '())
+(define *unparsed* '())
 
-     (define (parse input)
-       (set! *unparsed* input)
-       (let ((sent (parse-sentence)))
-         (require (null? *unparsed*))
-         sent))
+(define (parse input)
+  (set! *unparsed* input)
+  (let ((sent (parse-sentence)))
+    (require (null? *unparsed*))
+    sent))
+
+(define prepositions '(prep for to in by with))
+
+
+(define (parse-prepositional-phrase)
+  (list 'prep-phrase
+        (parse-word prepositions)
+        (parse-noun-phrase)))
+
+
+(define (parse-sentence)
+  (list 'sentence
+        (parse-noun-phrase)
+        (parse-verb-phrase)))
+
+(define (parse-verb-phrase)
+  (define (maybe-extend verb-phrase)
+    (amb verb-phrase
+         (maybe-extend (list 'verb-phrase
+                             verb-phrase
+                             (parse-prepositional-phrase)))))
+  (maybe-extend (parse-word verbs)))
+
+
+(define (parse-simple-noun-phrase)
+  (list 'simple-noun-phrase
+        (parse-word articles)
+        (parse-word nouns)))
+
+(define (parse-noun-phrase)
+  (define (maybe-extend noun-phrase)
+    (amb noun-phrase
+         (maybe-extend (list 'noun-phrase
+                             noun-phrase
+                             (parse-prepositional-phrase)))))
+  (maybe-extend (parse-simple-noun-phrase)))
+
 
 (parse '(the cat eats))
-     ;;; Starting a new problem
-     ;;; Amb-Eval value:
-     (sentence (noun-phrase (article the) (noun cat)) (verb eats))
+;;; Starting a new problem
+;;; Amb-Eval value:
+;; (sentence (noun-phrase (article the) (noun cat)) (verb eats))
+
+(parse '(the student with the cat sleeps in the class))
