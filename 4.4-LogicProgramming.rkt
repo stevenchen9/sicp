43505bbe05c3455a388b07125e471cc998f6d2a4
Steve Shogren <steve.a.shogren@gmail.com>
2014-08-03 12:46:01 -0400

    Some more query practice

--- 4.4-LogicProgramming.rkt
+++ 4.4-LogicProgramming.rkt
@@ -2,11 +2,40 @@
 ;;  information from the data base:
 ;;    1. all people supervised by Ben Bitdiddle;
 
-     (supervisor ?x (Bitdiddle Ben))
+(supervisor ?x (Bitdiddle Ben))
 ;;    2. the names and jobs of all people in the accounting division;
 
-     (job ?x (accounting . ?y))
+(job ?x (accounting . ?y))
 ;;    3. the names and addresses of all people who live in Slumerville.
-     (address ?x (Slumerville . ?y))
+(address ?x (Slumerville . ?y))
 
+
+; (salary ?person ?amount)
+; (address ?person ?where)
+; (supervisor ?person ?super)
+; (job ?person ?title)
+
+;;   *Exercise 4.56:* Formulate compound queries that retrieve the
+;;   following information:
+
+;;     a. the names of all people who are supervised by Ben Bitdiddle,
+;;        together with their addresses;
+
+(and (supervisor ?person (Bitdiddle Ben))
+     (address ?person ?where))
+
+;;     b. all people whose salary is less than Ben Bitdiddle's,
+;;        together with their salary and Ben Bitdiddle's salary;
+
+(or (salary (Bitdiddle Ben) ?bens)
+    (and (salary ?person ?amount)
+         (lisp-value > ?amount ?bens)))
+
+;;     c. all people who are supervised by someone who is not in the
+;;        computer division, together with the supervisor's name and
+;;        job.
+
+(and (supervisor ?person ?super)
+     (job ?super ?supertitle)
+     (not (job ?super (computer . ?x))))
 
