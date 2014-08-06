d596aca38ee72d44a8fd96be2212eb21a36206dd
Steve Shogren <steve.a.shogren@gmail.com>
2014-08-06 07:07:53 -0400

    Using compound queries to build abstractions

--- 4.4-LogicProgramming.rkt
+++ 4.4-LogicProgramming.rkt
@@ -10,10 +10,6 @@
 (address ?x (Slumerville . ?y))
 
 
-; (salary ?person ?amount)
-; (address ?person ?where)
-; (supervisor ?person ?super)
-; (job ?person ?title)
 
 ;;   *Exercise 4.56:* Formulate compound queries that retrieve the
 ;;   following information:
@@ -39,3 +35,37 @@
      (job ?super ?supertitle)
      (not (job ?super (computer . ?x))))
 
+; (salary ?person ?amount)
+; (address ?person ?where)
+; (supervisor ?person ?super)
+; (job ?person ?title)
+
+;;   *Exercise 4.57:* Define a rule that says that person 1 can replace
+;;   person 2 if either person 1 does the same job as person 2 or
+;;   someone who does person 1's job can also do person 2's job, and if
+;;   person 1 and person 2 are not the same person. Using your rule,
+;;   give queries that find the following:
+;;     a. all people who can replace Cy D. Fect;
+(rule (samejob ?p1 ?p2)
+      (and (job ?p1 ?p2job)
+           (job ?p2 ?p1job)
+           (same ?p1job ?p2job)))
+(rule (can-replace ?er ?ee)
+      (and (or (samejob ?er ?ee)
+               (and (samejob ?er ?other)
+                    (can-replace ?other ?ee)))
+           (not (same ?er ?ee))))
+
+(can-replace (Cy D. Fect) ?p)
+
+;;     b. all people who can replace someone who is being paid more
+;;        than they are, together with the two salaries.
+(rule (is-paid-less ?p1 ?p2)
+      (and (salary ?p1 ?p1Sal)
+           (salary ?p2 ?p2Sal)
+           (lisp-value < ?p1Sal ?p2Sal)))
+
+(and (can-replace ?p1 ?p2)
+     (is-paid-less ?p1 ?p2)
+     (salary ?p1 ?p1Sal)
+     (salary ?p2 ?p2Sal))
