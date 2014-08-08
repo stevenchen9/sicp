331564924e49db22396fef18afe121a2cf129c9e
Steve Shogren <steve.a.shogren@gmail.com>
2014-08-08 06:56:45 -0400

    Explaination for why the duplicate rows return

--- 4.4-LogicProgramming.rkt
+++ 4.4-LogicProgramming.rkt
@@ -128,3 +128,27 @@
 ;; (supervisor ?person ?super)
 ;; (job ?person ?title)
 ;; (meeting ?div (?day ?time))
+
+
+;;   *Exercise 4.60:* By giving the query
+
+;;        (lives-near ?person (Hacker Alyssa P))
+
+;;   Alyssa P. Hacker is able to find people who live near her, with
+;;   whom she can ride to work.  On the other hand, when she tries to
+;;   find all pairs of people who live near each other by querying
+
+;;        (lives-near ?person-1 ?person-2)
+
+;;   she notices that each pair of people who live near each other is
+;;   listed twice; for example,
+
+;;        (lives-near (Hacker Alyssa P) (Fect Cy D))
+;;        (lives-near (Fect Cy D) (Hacker Alyssa P))
+
+;;   Why does this happen?  Is there a way to find a list of people who
+;;   live near each other, in which each pair appears only once?
+;;   Explain.
+
+This happens because the pairs are unordered. A way to fix this would
+be to order the return and then only take the top half.
