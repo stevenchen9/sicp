9dea83615034c40b885d3abdecdf3c141619bbcf
Steve Shogren <steve.a.shogren@gmail.com>
2014-08-09 08:32:05 -0400

    Solving for the new next-to rules

--- 4.4-LogicProgramming.rkt
+++ 4.4-LogicProgramming.rkt
@@ -152,3 +152,22 @@
 
 This happens because the pairs are unordered. A way to fix this would
 be to order the return and then only take the top half.
+
+
+;;   *Exercise 4.61:* The following rules implement a `next-to'
+;;   relation that finds adjacent elements of a list:
+
+;;        (rule (?x next-to ?y in (?x ?y . ?u)))
+
+;;        (rule (?x next-to ?y in (?v . ?z))
+;;              (?x next-to ?y in ?z))
+
+;;   What will the response be to the following queries?
+
+;;        (?x next-to ?y in (1 (2 3) 4))
+((2 3) next-to 4 in (1 (2 3) 4))
+(1 next-to (2 3) in (1 (2 3) 4))
+
+;;        (?x next-to 1 in (2 1 3 1))
+(2 next-to 1 in (2 1 3 1))
+(3 next-to 1 in (2 1 3 1))
