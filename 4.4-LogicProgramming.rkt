f9df6162b922e7d7c9b3c94c2efb60c852f5843f
Steve Shogren <steve.a.shogren@gmail.com>
2014-08-10 08:03:38 -0400

    Adding rules for a list-pair

--- 4.4-LogicProgramming.rkt
+++ 4.4-LogicProgramming.rkt
@@ -171,3 +171,24 @@ be to order the return and then only take the top half.
 ;;        (?x next-to 1 in (2 1 3 1))
 (2 next-to 1 in (2 1 3 1))
 (3 next-to 1 in (2 1 3 1))
+
+
+;;   *Exercise 4.62:* Define rules to implement the `last-pair'
+;;   operation of *Note Exercise 2-17::, which returns a list
+;;   containing the last element of a nonempty list.  Check your rules
+;;   on queries such as `(last-pair (3) ?x)', `(last-pair (1 2 3) ?x)',
+;;   and `(last-pair (2 ?x) (3))'.  Do your rules work correctly on
+;;   queries such as `(last-pair ?x (3))' ?
+(rule (last-pair (?x) (?x)))
+(rule (last-pair (?u . ?v) (?x))
+      (last-pair ?v (?x)))
+
+(last-pair (3) ?x)
+;; => (last-pair (3) 3)
+(last-pair (1 2 3) ?x)
+;; => (last-pair (1 2 3) 3)
+(last-pair (2 ?x) (3))
+;; =? (last-pair (2 3) (3))
+
+
+
