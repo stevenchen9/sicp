c691c9c4a4c22372d7c368f4c1f90d0c2e680e51
Steve Shogren <steve.a.shogren@gmail.com>
2014-08-25 08:57:51 -0400

    Work for the 2.2.1 presentation

--- 2.2.1-2.2.2-presentation.rkt
+++ 2.2.1-2.2.2-presentation.rkt
@@ -21,3 +21,41 @@
 
 
 
+(define (length items)
+  (if (null? items)
+      0
+      (+ 1 (length (cdr items)))))
+
+(length '(1 2 2))
+
+(define (append list1 list2)
+  (if (null? list1)
+      list2
+      (cons (car list1) (append (cdr list1) list2))))
+(append '(1 2 3) '(4 5 6))
+
+
+
+(define (scale-list items factor)
+  (if (null? items)
+      null
+      (cons (* (car items) factor)
+            (scale-list (cdr items) factor))))
+
+(scale-list (list 1 2 3 4 5) 10)
+
+
+
+(define (map proc items)
+  (if (null? items)
+      null
+      (cons (proc (car items))
+            (map proc (cdr items)))))
+
+(map abs (list -10 2.5 -11.6 17))
+
+
+
+(define (scale-list items factor)
+  (map (lambda (x) (* x factor))
+       items))
