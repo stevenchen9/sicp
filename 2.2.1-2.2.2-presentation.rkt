fb5df4e032986af118365c8982022ecd877f6ab1
Steve Shogren <steve.a.shogren@gmail.com>
2014-09-05 07:05:18 -0400

    Working on the logic of how the evaluator reads in a query

--- 2.2.1-2.2.2-presentation.rkt
+++ 2.2.1-2.2.2-presentation.rkt
@@ -1,3 +1,21 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ;; Lists 
 
 ;;       +---+---+     +---+---+     +---+---+     +---+---+
@@ -15,10 +33,40 @@
                        (cons 3
                              (cons 4 null)))))
 
+
+
+
+
+
+
+
+
+
+
 (define l2 (list 1 2 3 4))
 
-(define l3 '(1 2 3 4))
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+(define l3 (1 2 3 4))
+
+
+
+
+(car (list 1 2))
+(cdr (list 1 2 3 4))
 
 
 (define (length items)
@@ -26,13 +74,54 @@
       0
       (+ 1 (length (cdr items)))))
 
+(define (length2 items)
+  (define (length-h items accum)
+    (if (null? items)
+        accum
+        (length-h (cdr items) (+ 1 accum))))
+  (length-h items 0))
+
+
 (length '(1 2 2))
+(length2 '(1 2 2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 (define (append list1 list2)
   (if (null? list1)
       list2
       (cons (car list1) (append (cdr list1) list2))))
+(cons 1
+      (cons 2
+            (cons 3
+                  '(4 5 6))))
 (append '(1 2 3) '(4 5 6))
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -46,6 +135,15 @@
 
 
 
+
+
+
+
+
+
+
+
+(+ 1 1 () 3)
 (define (map proc items)
   (if (null? items)
       null
@@ -59,6 +157,17 @@
 (define (scale-list items factor)
   (map (lambda (x) (* x factor))
        items))
+
+
+(scale-list (list 1 2 3 4 5) 10)
+
+
+
+
+
+
+
+
 
 ;; Trees
 ;;                                            (3 4)
@@ -78,14 +187,39 @@
 ;;               | 1 |         | 2 |
 ;;               +---+         +---+
 
+
+
+
+
+
 (define x (cons (list 1 2) (list 3 4)))
 (length x) ;; => 3
+
+
+
+
+
+
+
+
+
+
 
 (define (count-leaves x)
   (cond ((null? x) 0)
         ((not (pair? x)) 1)
         (else (+ (count-leaves (car x))
                  (count-leaves (cdr x))))))
+
+
+(count-leaves x)
+
+
+
+
+
+
+
 
 
 (define (scale-tree tree factor)
@@ -97,9 +231,20 @@
 (scale-tree x 10)
 
 
+
+
+
+
+
+
+
+
+
+
 (define (scale-tree tree factor)
   (map (lambda (sub-tree)
          (if (pair? sub-tree)
              (scale-tree sub-tree factor)
              (* sub-tree factor)))
        tree))
+(scale-tree x 10)
--- 4.4-LogicProgramming.rkt
+++ 4.4-LogicProgramming.rkt
@@ -192,3 +192,24 @@ be to order the return and then only take the top half.
 
 
 
+;;   *Exercise 4.64:* Louis Reasoner mistakenly deletes the
+;;   `outranked-by' rule (section *Note 4-4-1::) from the data base.
+;;   When he realizes this, he quickly reinstalls it.  Unfortunately,
+;;   he makes a slight change in the rule, and types it in as
+
+(rule (outranked-by ?staff-person ?boss)
+      (or (supervisor ?staff-person ?boss)
+          (and (outranked-by ?middle-manager ?boss)
+               (supervisor ?staff-person ?middle-manager))))
+
+;;   Just after Louis types this information into the system, DeWitt
+;;   Aull comes by to find out who outranks Ben Bitdiddle. He issues
+;;   the query
+
+;;        (outranked-by (Bitdiddle Ben) ?who)
+
+;;   After answering, the system goes into an infinite loop.  Explain
+;;   why.
+
+;; The two or queries are evaluated in parallel, which causes the
+;; infinite loop
