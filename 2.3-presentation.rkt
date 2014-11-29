80c13e9a1f4686f26727185d328ca1c7d9279c51
Steve Shogren <steve.a.shogren@gmail.com>
2014-11-29 15:45:29 -0500

    Unordered sets

--- 2.3-presentation.rkt
+++ 2.3-presentation.rkt
@@ -101,3 +101,23 @@
         ((=number? m2 1) m1)
         ((and (number? m1) (number? m2)) (* m1 m2))
         (else (list '* m1 m2))))
+
+
+
+;; sets as unordered
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((equal? x (car set)) true)
+        (else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set)
+  (if (element-of-set? x set)
+      set
+      (cons x set)))
+
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) '())
+        ((element-of-set? (car set1) set2)
+         (cons (car set1)
+               (intersection-set (cdr set1) set2)))
+        (else (intersection-set (cdr set1) set2))))
