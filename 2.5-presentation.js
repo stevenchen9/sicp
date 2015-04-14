7bbdab56b661914dd77d1a8aacd289f4791c8167
Steve Shogren <steve.a.shogren@gmail.com>
2015-04-14 17:55:22 -0400

    More work on the js dispatcher

--- 2.5-presentation.js
+++ 2.5-presentation.js
@@ -1,16 +1,21 @@
 var dispatch = {}; 
 var put = function(op, type, f){
-    dispatch[op][type] = f;
+    dispatch[op + type] = f;
 };
 var get = function(op, type){
-    return dispatch[op][type]; 
+    return dispatch[op + type]; 
+};
+var type_tag = function(obj){
+    return obj[0];
+};
+var contents = function(obj){
+    return obj[1];
 };
 var attach_tag = function(tag, obj){
-    obj.unshift(tag);
-    return obj;
+    return [tag, obj];
 };
 
-var installPolarPackage = function(){
+var installRectPackage = function(){
   var real_part = function(z) { return z[0];};
   var imag_part = function(z) { return z[1];};
   var make_from_real_imag = function(x, y) { return [x, y];};
@@ -35,4 +40,42 @@ var installPolarPackage = function(){
        function(x, y){ return tag(make_from_mag_ang(x, y));});
 }; 
 
-console.log("tadf");
+var apply_generic = function(op, arg){
+    var type_tags = type_tag(arg);
+    var proc = get(op, type_tags);
+    if (proc) {
+        return proc(contents(arg));
+    } else {
+        return "No method for these types -- APPLY-GENERIC";
+    }
+};
+
+var real_part = function(z){return apply_generic('real_part', z);};
+var imag_part = function(z){return apply_generic('imag_part', z);};
+var magnitude = function(z){return apply_generic('magnitude', z);};
+var angle = function(z){return apply_generic('angle', z);};
+
+var make_from_real_imag = function (x, y) {
+    return get('make_from_real_imag', 'rectangular')(x, y);
+};
+
+installRectPackage();
+
+var rect1 = make_from_real_imag(2, 2);
+var rect2 = make_from_real_imag(7, 0);
+
+
+var add_complex = function(z1, z2) {
+  return make_from_real_imag((real_part(z1) + real_part(z2)),
+                             (imag_part(z1) + imag_part(z2)));
+};
+
+console.log("contesnt: " + contents(rect2));
+console.log("type_tag: " + type_tag(rect2));
+console.log("Real: " + real_part(rect2));
+console.log("Image: " + imag_part(rect2));
+console.log("mag: " + magnitude(rect2));
+console.log("ang: " + angle(rect2));
+console.log("sum: " + add_complex(rect1, rect2));
+
+
